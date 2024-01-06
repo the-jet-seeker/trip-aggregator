1765972dce48ba64f84ba5a5bb417f88e1770ce3
Irina Eiduk <eira@mail.ru>
2024-01-06 15:22:40 +0100

    actual tickets db structure added

--- etc/fixture.sql
+++ etc/fixture.sql
@@ -16,4 +16,19 @@ CREATE TABLE IF NOT EXISTS "public"."ticket" (
 ) WITH (oids = false);
 
 CREATE INDEX IF NOT EXISTS "ticket_source" ON "public"."ticket" USING btree ("source");
+
 CREATE INDEX IF NOT EXISTS "ticket_dep_datetime" ON "public"."ticket" USING btree ("dep_datetime");
+
+ALTER TABLE "public"."ticket" ADD COLUMN IF NOT EXISTS "uid" character varying(255) NOT NULL DEFAULT '';
+
+UPDATE ticket SET uid=ticket.flight_number || '_' || ticket.from_airport_code || '_' || ticket.to_airport_code || '_' || ticket.dep_datetime  where uid = '';
+
+CREATE UNIQUE INDEX  IF NOT EXISTS "ticket_uid" ON  "public"."ticket" ("uid");
+
+ALTER TABLE "public"."ticket" ADD COLUMN IF NOT EXISTS "arr_datetime" timestamp NOT NULL DEFAULT '2020-01-01';
+
+UPDATE ticket SET arr_datetime = dep_datetime WHERE arr_datetime = '2020-01-01';
+
+ALTER TABLE "public"."ticket" ALTER COLUMN "arr_datetime" DROP DEFAULT;
+
+CREATE INDEX IF NOT EXISTS "ticket_arr_datetime" ON "public"."ticket" USING btree ("arr_datetime");
