58e29dd8c18a54e4729fe78eb4caed1483f3e7b1
Irina Eiduk <eira@mail.ru>
2024-01-08 11:23:13 +0100

    adding fixtures

--- tests/trip_aggregator/storage/tickets/test_fetch_tickets.py
+++ tests/trip_aggregator/storage/tickets/test_fetch_tickets.py
@@ -1,14 +1,40 @@
+from datetime import timedelta
+
+import pendulum
+
 from trip_aggregator.run_task import _weekend_interval
 from trip_aggregator.settings import app_settings
 from trip_aggregator.storage.tickets import fetch_tickets
 
 
-def test_fetch_tickets_smoke():
-    # todo fixture with tickets and clean after
-    weekend_date = _weekend_interval()
-    home_airport = app_settings.HOME_AIRPORT
-
-    res = fetch_tickets(weekend_date, home_airport)
+def test_fetch_tickets_happy_path(empty_tickets):
+    res = fetch_tickets(_weekend_interval(), app_settings.HOME_AIRPORT)
 
     assert isinstance(res, list)
-    #assert isinstance(res[0], models.Ticket)
+    assert len(res) == 0
+
+
+def test_fetch_tickets(empty_tickets, ticket):
+    res = fetch_tickets(
+        pendulum.Interval(start=ticket.dep_datetime, end=ticket.arr_datetime),
+        ticket.from_airport_code,
+    )
+
+    assert len(res) == 1
+    assert res[0].id == ticket.id
+
+
+def test_fetch_tickets_filter_dep_date(empty_tickets, ticket):
+    res = fetch_tickets(
+        pendulum.Interval(start=ticket.dep_datetime + timedelta(seconds=1), end=ticket.arr_datetime),
+        ticket.from_airport_code,
+    )
+
+    assert len(res) == 0
+
+def test_fetch_tickets_filter_arr_date(empty_tickets, ticket):
+    pass
+
+
+def test_fetch_tickets_filter_airport(empty_tickets, ticket):
+    pass
